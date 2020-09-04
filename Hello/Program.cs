1b4e84ad8ca2b0f9bbe00a7217480af6513d3b81
Przemek Mroz <frost22a@gmail.com>
2020-09-04 11:24:19 +0200

    up

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -4,6 +4,7 @@ namespace Hello
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -13,6 +14,7 @@ namespace Hello
                 Console.WriteLine("Witaj " + item);
             }
             Console.WriteLine("elo!");           
+            
 
         }
     }
---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //Stan -> zmienne (pola)
+        public List<float> Ratings { get; set; }
+
+
+
+        //Zachowania -> metody 
+        public void AddRating(float rating) 
+        {
+            Ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            var sum = (float)0;
+            var avg = (float)0;
+            
+            foreach (var rating in Ratings)
+            {
+                sum = +rating;
+            }
+
+            avg = sum / Ratings.Count();
+            return avg;
+        }
+
+        public float GiveMaxRating() 
+        {
+            return Ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return Ratings.Min();
+        }
+    }
+}
