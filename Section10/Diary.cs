4f36e011eff2631bec276cccc2edcd2bf3864a3b
Przemek Mroz <frost22a@gmail.com>
2020-09-04 22:27:26 +0200

    diff between class and variable

---
+++ Section10/Diary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Section10
+{
+    class Diary
+    {
+        //Stan -> zmienne (pola) -> trzeba zaincjować zmienną - > w tym wypadku new 
+        List<float> Ratings = new List<float>();
+        
+        //Zachowania -> metody 
+        public void AddRating(float rating) 
+        {
+            Ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            var sum = (float)0;
+            var avg = (float)0;
+            
+            foreach (var rating in Ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / Ratings.Count();
+            return avg;
+        }
+
+        public float GiveMaxRating() 
+        {
+            return Ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return Ratings.Min();
+        }
+
+    }
+}
---
+++ Section10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Section10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // towrzymy zmienną diary typu Diary: 
+            // Diary diary; 
+            // jej wartość to w tej chwili null -> zmienna jest jakby wskaźnikiem do obiektu
+            // teraz zainicjujemy obiekt:
+            Diary diary = new Diary();
+            // zainicjowaliśmy obiekt klasy Diary - mamy utworzoną nową instancję klasy Diary
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+            Diary diary2 = diary;
+            diary2.AddRating(8);
+
+            Diary diary3 = diary2;
+            diary3.AddRating(9);
+
+        }
+    }
+}
