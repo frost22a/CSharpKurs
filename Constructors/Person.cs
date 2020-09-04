40a6fabcdc91ed09d2bd3e810b1d50024391954a
Przemek Mroz <frost22a@gmail.com>
2020-09-04 21:38:10 +0200

    make few examples of constructors

---
+++ Constructors/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Person
+    {
+        // właściowości:
+        int Id;
+        string FirstName;
+        string LastName;
+        string Adrress;
+        int Age;
+        string Gender;
+
+        public Person() // konstruktor, który przyjmuje warości domyślne -> jak w przykładzie: 
+        {
+            Id = 1;
+            FirstName = "Przemek";
+            LastName = "Mróz";
+            Age = 39;
+        }
+
+
+        // konstruktor, który wywoływany jest z parametrami: 
+        public Person(int Id, string FirstName, string LastName, string Adrress, int Age, string Gender)
+        {
+            this.Id = Id; // słowo this oznacza, że przypisujemy do właściwości Id wartość zmiennej Id z konstruktora
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Adrress = Adrress;
+            this.Age = Age;
+            this.Gender = Gender;
+
+        }
+
+        public Person(int Id, string FirstName, string LastName)
+        {
+            this.Id = Id; // słowo this oznacza, że przypisujemy do właściwości Id wartość zmiennej Id z konstruktora
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Adrress = "Łąkie";
+            this.Age = 18;
+        }
+
+        public Person(int Id, string FirstName)
+        {
+            this.Id = Id; // słowo this oznacza, że przypisujemy do właściwości Id wartość zmiennej Id z konstruktora
+            this.FirstName = FirstName;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tworzymy obiekt klasy person:
+            Person person1 = new Person();
+            Person person2 = new Person(1, "Tomek");
+            Person person3 = new Person(2, "Tomek", "Nowak");
+            Person person4 = new Person(2, "Tomek", "Nowak", "Katowice", 18, "mężczyzna");
+
+        }
+    }
+}
